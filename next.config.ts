6b94d1d976473242d956d1e6b1ff7ec49c18d08a
Soumyajit001 <soumyajitpal416@gmail.com>
2026-05-05 12:34:13 +0530

    small changes in frontend

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    allowedDevOrigins: ["192.168.1.15:3000", "localhost:3000"]
-  }
 };
 
 export default nextConfig;
